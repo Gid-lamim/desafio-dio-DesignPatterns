71422b729e3c20335ab4f5dbe8b381ebdfebb6e1
EuMesmo <gid.lamim@gmail.com>
2022-02-18 11:35:53 -0300

    singletonLazy

--- src/one/digitalinnovation/gof/SingletonLazy.java
+++ src/one/digitalinnovation/gof/SingletonLazy.java
@@ -1,4 +1,32 @@
 package one.digitalinnovation.gof;
 
+/**
+ *
+ * singleton "preguiçoso"
+ * @author: gidLamim
+ *
+ * Singleton Java :
+ *      Singleton is a creational design pattern, which ensures that only
+ *      one object of its kind exists and provides a single point of access
+ *      to it for any other code. Singleton has almost the same pros and cons
+ *      as global variables. Although they're super-handy, they break the
+ *      modularity of your code.
+ *
+ */
+
 public class SingletonLazy {
+
+    private static SingletonLazy intancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia(){
+        if (intancia == null){
+            intancia= new SingletonLazy();
+        }
+
+        return intancia;
+    }
 }
